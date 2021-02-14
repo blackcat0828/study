d1d9b61b0b685bfc7d9be407d14b588278ec6215
Jeff Kim <watb88@gmail.com>
2021-02-14 22:41:16 +0900

    0214

--- Spring5/sp5-chap07-gradle/src/main/java/chap07/ImpeCalculator.java
+++ Spring5/sp5-chap07-gradle/src/main/java/chap07/ImpeCalculator.java
@@ -4,7 +4,7 @@ public class ImpeCalculator implements Calculator {
 	@Override
 	public long factorial(long num) {
 		long result = 1;
-		for (int i = 0; i <= num; i++) {
+		for (int i = 1; i <= num; i++) {
 			result *= i;
 		}
 		return result;
